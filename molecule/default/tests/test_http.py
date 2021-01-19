bc6f96febe4bc12c9b4ac7028181a65e259e2422
Rafael Gomes <linux.rafa@gmail.com>
2021-01-19 20:52:02 -0300

    Não precisamos mais do socket 443

--- molecule/default/tests/test_http.py
+++ molecule/default/tests/test_http.py
@@ -10,8 +10,3 @@ def test_is_http_listening(host):
     socket_http = host.socket("tcp://0.0.0.0:80")
  
     assert socket_http.is_listening 
-
-def test_is_https_listening(host):
-    socket_https = host.socket("tcp://0.0.0.0:443")
- 
-    assert socket_https.is_listening 
